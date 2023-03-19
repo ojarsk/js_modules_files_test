915538deda7e732def76987163df66430e4d627c
ojarsk <98034234+ojarsk@users.noreply.github.com>
2023-03-19 17:46:28 +0000

    Draw player

--- main.js
+++ main.js
@@ -1,20 +1,35 @@
+import {Player} from './player.js'
+
+window.addEventListener('load', start );
+
 function start() {
     const canvas = document.getElementById("myCanvas");
     const ctx = canvas.getContext('2d');
 
-    gmae = new Game();
+    let game = new Game(canvas);
+
+    function animate() {
+        console.log(ctx);
+        game.draw(ctx);
+        requestAnimationFrame(animate);
+    }
+    animate();
+    
 }
 
+
 class Game {
-    constructor( canvas ) {
+    constructor(canvas) {
         this.canvas = canvas;
         this.width = canvas.width;
         this.height = canvas.height;
+        this.player = new Player(this, 10, 10, 20, 20);
     }
     update() {
 
     }
-    draw() {
-        
+    draw(context) {
+        this.player.draw(context);
     }
 }
+
---
+++ player.js
@@ -0,0 +1,15 @@
+export class Player {
+    constructor(game, x, y, w, h) {
+        this.game = game;
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+    }
+    update() {
+
+    }
+    draw(context) {
+        context.fillRect( this.x, this.y, this.w, this.h );
+    }
+}
